f8c844fc4847039974396747a7280eae9a300544
Sumer Kohli <sumer.kohli@berkeley.edu>
2022-08-23 19:26:00 -0700

    refactor(game): separate answer/guess lists, better pretty-printing

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, iter::Zip, sync::Arc};
+use console::{style, Color, Term};
+use std::{collections::BTreeMap, fmt::Display, iter::Zip, sync::Arc};
 
 use crate::{
     strategy::{Strategy, StrategyVerbosity},
@@ -16,11 +17,39 @@ pub enum TileOutcome {
     Green,
 }
 
+impl TileOutcome {
+    pub fn color(&self) -> Color {
+        match self {
+            TileOutcome::Gray => Color::Color256(242), // Grey42 (#6c6c6c)
+            TileOutcome::Green => Color::Green,
+            TileOutcome::Yellow => Color::Yellow,
+        }
+    }
+}
+
 /// Represents a guess and its paired outcome (i.e. gray/green/yellow tiles).
 #[derive(Default)]
 pub struct Guess {
     pub guess: Vec<char>,
     pub outcome: Vec<TileOutcome>,
+}
+
+impl Display for Guess {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (ch, outcome) in self.guess.iter().zip(self.outcome.iter()) {
+            write!(
+                f,
+                "{}",
+                style(ch.to_ascii_uppercase())
+                    .fg(Color::White)
+                    .bg(outcome.color())
+                    .bright()
+                    .bold()
+            )?;
+        }
+
+        Ok(())
+    }
 }
 
 impl Guess {
@@ -36,8 +65,9 @@ impl Guess {
 pub struct Game {
     word: WordPtr,
     guesses: Vec<Box<Guess>>,
-    wordlist: WordlistPtr,
-    history: Vec<HashMap<String, f64>>,
+    answerlist: WordlistPtr,
+    guesslist: WordlistPtr,
+    history: Vec<BTreeMap<String, f64>>,
     strategy: Box<dyn Strategy>,
 }
 
@@ -52,34 +82,38 @@ pub enum GameState {
 impl Game {
     /// Initializes a new Game with the given `wordlist` and strategy initialization function.
     pub fn init(
-        wordlist: WordlistPtr,
+        guesslist: WordlistPtr,
+        answerlist: WordlistPtr,
         strategy_init: &dyn Fn(WordlistPtr) -> Box<dyn Strategy>,
     ) -> Self {
         let mut game = Game {
             word: Arc::default(),
             guesses: vec![],
-            wordlist: wordlist.clone(),
+            answerlist,
+            guesslist: guesslist.clone(),
             history: vec![],
-            strategy: strategy_init(wordlist),
+            strategy: strategy_init(guesslist),
         };
         game.push_metrics();
         game
     }
 
-    /// Choose a word at random from the wordlist.
+    /// Choose a word at random from the answer list.
+    #[allow(dead_code)]
     pub fn choose_random_word(&mut self) {
         self.word = self
-            .wordlist
+            .answerlist
             .random_word()
-            .expect("Could not choose word from empty list!");
+            .expect("Could not choose random word from empty answer list!");
     }
 
-    /// Choose the given word.
+    /// Choose the given word (must only be in the guess list, not answer list).
+    #[allow(dead_code)]
     pub fn choose_word(&mut self, word: &str) {
         self.word = self
-            .wordlist
+            .guesslist
             .get_word(word)
-            .expect("Given word is not in wordlist!");
+            .expect("Given word is not in guess list!");
     }
 
     /// Make a given guess.
@@ -104,33 +138,48 @@ impl Game {
     }
 
     /// Pretty-print game state.
-    pub fn pretty_print(&self) {
-        let divider = "+---".to_string().repeat(20);
-        println!();
-        println!("{}", divider);
-        println!("Chosen word: {}", self.word);
-        println!("# Guesses: {}", self.guesses.len());
-        println!(
-            "# Allowed Guesses: {} (entropy: {})",
-            self.allowed_guesses().len(),
-            self.wordlist.unweighted_entropy()
-        );
+    pub fn pretty_print(&self) -> Result<(), std::io::Error> {
+        let term = Term::stdout();
 
-        println!("{}", divider);
+        term.write_line(
+            &style("CRUSTACEAN WORDLE")
+                .cyan()
+                .bright()
+                .bold()
+                .underlined()
+                .to_string(),
+        )?;
+        term.move_cursor_down(1)?;
+
+        term.write_line(
+            format!(
+                "Game is {}.",
+                style(match self.current_state() {
+                    GameState::Invalid => "an invalid state",
+                    GameState::NotStarted => "starting",
+                    GameState::InProgress => "in progress",
+                    GameState::GuesserDefeat | GameState::GuesserVictory => "over",
+                })
+                .bold()
+            )
+            .as_str(),
+        )?;
+        term.move_cursor_down(1)?;
+
         for (idx, guess) in self.guesses.iter().enumerate() {
-            let guessed_word: String = guess.guess.iter().collect();
-            println!("Guess #{}: {} [{:?}]", idx + 1, guessed_word, guess.outcome);
+            term.write_line(format!("#{}: {}", idx + 1, guess).as_str())?;
         }
 
-        println!("{}", divider);
-        self.strategy.pretty_print(&self.history);
+        if self.guesses.is_empty() {
+            term.write_line("No guesses yet.")?;
+        }
 
-        println!("{}", divider);
+        Ok(())
     }
 
     /// Return all allowed guesses according to the wordlist.
     pub fn allowed_guesses(&self) -> Vec<WordPtr> {
-        self.wordlist.possible_words().to_vec()
+        self.guesslist.possible_words().to_vec()
     }
 
     /// Return all extant guesses according to the strategy.
@@ -177,5 +226,43 @@ impl Game {
     /// Set strategy verbosity.
     pub fn set_verbosity(&mut self, verbosity: StrategyVerbosity) {
         self.strategy.set_verbosity(verbosity)
+    }
+
+    /// Retrieve wordlist.
+    pub fn get_wordlist(&self) -> WordlistPtr {
+        self.guesslist.clone()
+    }
+}
+
+impl Display for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let divider = "+---".to_string().repeat(20);
+        writeln!(f)?;
+        writeln!(f, "{}", divider)?;
+        writeln!(f, "Chosen word: {}", self.word)?;
+        writeln!(f, "# Guesses: {}", self.guesses.len())?;
+        writeln!(
+            f,
+            "# Allowed Guesses: {} (entropy: {})",
+            self.allowed_guesses().len(),
+            self.guesslist.unweighted_entropy()
+        )?;
+
+        writeln!(f, "{}", divider)?;
+        for (idx, guess) in self.guesses.iter().enumerate() {
+            let guessed_word: String = guess.guess.iter().collect();
+            writeln!(
+                f,
+                "Guess #{}: {} [{:?}]",
+                idx + 1,
+                guessed_word,
+                guess.outcome
+            )?;
+        }
+
+        writeln!(f, "{}", divider)?;
+        self.strategy.pretty_print(&self.history);
+
+        writeln!(f, "{}", divider)
     }
 }
